7c92a3a9ece0489a1cfc3efd19e8cc39ecf5bcd8
Oliver Cadman <o.cadman@live.co.uk>
2023-01-09 15:18:37 +0000

    Configure HTTPS

--- proxy/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
-
-RED='\033[0;31m'
-echo -e "${RED}Remember to update DJANGO_ALLOWED_HOSTS with the new AWS URL!!${NC}"
