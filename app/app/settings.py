b7d7301381ab0d2d3914030eee266d62f6dfccb5
Oliver Cadman <o.cadman@live.co.uk>
2023-01-09 22:02:18 +0000

    Set DEBUG to True

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 ALLOWED_HOSTS.extend(
