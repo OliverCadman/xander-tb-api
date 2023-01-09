5d6953942966ed77e0235d2a5337af4bc61ffc5c
Oliver Cadman <o.cadman@live.co.uk>
2023-01-09 22:45:27 +0000

    Remove latitude, longitude and country fields from postcode models

--- app/core/models.py
+++ app/core/models.py
@@ -68,9 +68,6 @@ class DeliveryPostcode(models.Model):
 
     postcode = models.CharField(max_length=20)
     postcode_area = models.CharField(max_length=5, null=True)
-    country = models.CharField(max_length=30, null=True)
-    latitude = models.DecimalField(decimal_places=8, max_digits=15, null=True)
-    longitude = models.DecimalField(decimal_places=8, max_digits=15, null=True)
 
     def __str__(self):
         return self.postcode
@@ -92,10 +89,6 @@ class BillingPostcode(models.Model):
 
     postcode = models.CharField(max_length=20)
     postcode_area = models.CharField(max_length=5, null=True)
-    country = models.CharField(max_length=30, null=True)
-    latitude = models.DecimalField(decimal_places=8, max_digits=15, null=True)
-    longitude = models.DecimalField(decimal_places=8, max_digits=15, null=True)
-
 
     def __str__(self):
         return self.postcode
